d40b9a823fbf24f52b369c7669c3bd6a4572259b
SamWaku <samuelwaku1st@gmail.com>
2025-05-09 11:45:57 +0200

    chore: RouteDataRequired

--- Controllers/ModelBindingController.cs
+++ Controllers/ModelBindingController.cs
@@ -7,10 +7,19 @@ public class ModelBindingController : Controller
     //form data
 
     //Route data
+    //not required
     [HttpGet("/api/route-data/{id}/{name}")]
     public IActionResult RouteData(int? id, string? name) 
     {
         return Content($"Your id is {id} and name is {name}");
+    }
+
+    //Required id
+    [HttpGet("/api/route-data/{id}/{name}")]
+    public IActionResult RouteDataRequired() 
+    {
+        int id = Convert.ToInt32(Request.QueryString[])
+        return Content($"Your id is {id}");
     }
 
     //Query string
